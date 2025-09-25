f9cd24dad4787b0397a1d1d3e4c949ee7d01767e
shuaikai <wddjwk@qq.com>
2025-09-25 20:29:05 +0800

    [DEMO][FEATURE] add demo of gtest

--- test/gtest.cpp
+++ test/gtest.cpp
@@ -1,3 +1,18 @@
 #include "gtest/gtest.h"
 
-TEST(Hello, World) { ASSERT_EQ(1, 1); }
+class BaseTest : public ::testing::Test {
+public:
+    void SetUp() override {}
+
+    void TearDown() override {}
+
+    static void SetUpTestSuite() {}
+
+    static void TearDownTestSuite() {}
+
+protected:
+};
+
+TEST_F(BaseTest, World) {
+    ASSERT_EQ(1, 1);
+}
